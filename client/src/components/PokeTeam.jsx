90518559f5384f3fca11d7c6223131a3c90653f0
Jyumi <justinlu75@gmail.com>
2019-11-06 18:53:14 -0800

    added some solutions

--- client/src/components/PokeTeam.jsx
+++ client/src/components/PokeTeam.jsx
@@ -1,11 +1,11 @@
-//TODO
-/*
-<div id="pokeTeam">
-  PokemonTeamSlot
-  PokemonTeamSlot
-  PokemonTeamSlot
-  PokemonTeamSlot
-  PokemonTeamSlot
-  PokemonTeamSlot
-</div>
-*/
+import React from 'react';
+import PokeTeamSlot from './PokeTeamSlot.jsx';
+const PokeTeam = (props) => {
+  return (
+    <div id="pokeTeam">
+      {props.pokeTeam.map((pokemon, index) => (<PokeTeamSlot pokemon={pokemon} key={index}/>))}
+    </div>
+  )
+}
+
+export default PokeTeam;
--- client/src/components/PokeTeamSlot.jsx
+++ client/src/components/PokeTeamSlot.jsx
@@ -6,4 +6,18 @@
     <img src="" alt="pokemon image"/> 
   </div>
 */
-
+import React from 'react';
+import AddPokeForm from './AddPokeForm.jsx'
+const pokeTeamSlot = (props) => {
+(props.pokemon)
+  ? (
+    <div className="pokeTeamSlot">
+    <div>Name: {props.pokemon.name}</div>
+    <div>Id: {props.pokemon.id}</div>
+    <img src={props.pokemon.sprites.front_default} alt="pokemon image"/> 
+    </div>
+  ) : (
+    <AddPokeForm addPokeForm={props.addPokemon} index={props.key}/>
+  )
+}
+export default pokeTeamSlot;
